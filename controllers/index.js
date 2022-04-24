2f709b297fc1a004230f3c0fd0e5616f333766ff
aamott <adam.amott@gmail.com>
2022-04-23 22:54:56 -0600

    Display a name

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@
    res - response */
 displayJoke = (req, res) => {
     const data =
-        'How did the telephone propose to his girlfriend? ...he gave her a ring.';
+        'Ammon Richards';
     // send status, then send data. data doesn't have to be sent, it can 
     // just return res.status(200).
     res.status(200).send(data);
