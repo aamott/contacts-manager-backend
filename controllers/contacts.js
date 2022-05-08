10a4f8c12f17591b3de65d8dc4a59bb632610054
aamott <adam.amott@gmail.com>
2022-05-07 23:05:56 -0600

    Added CRUD endpoints

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,9 @@
 const mongodb = require( '../db/connect' );
 const ObjectId = require( 'mongodb' ).ObjectId;
 
+/************************
+ * GET ALL CONTACTS
+ */
 const getAll = async ( req, res, next ) => {
   // DEBUGGING
   // console.log("Getting all users")
@@ -13,8 +16,6 @@ const getAll = async ( req, res, next ) => {
 
 const getSingle = async ( req, res, next ) => {
   const userId = new ObjectId( req.params.id );
-  // DEBUGGING
-  // console.log("Getting single user: " + userId);
   const result = await mongodb
     .getDb()
     .db()
@@ -28,7 +29,96 @@ const getSingle = async ( req, res, next ) => {
   } );
 };
 
+
+/*************************
+ * POST NEW CONTACT
+ */
+const postNew = async ( req, res, next ) => {
+  if ( req.body.firstName && req.body.lastName &&
+    req.body.email && req.body.favoriteColor && req.body.birthday ) {
+    const newContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    }
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection( 'contacts' )
+      .insertOne( newContact );
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.status( 200 ).json( result.insertedId );
+  } else {
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.status( 400 ).json( {
+      error: 'Missing required fields'
+    } );
+  }
+}
+
+
+/*************************
+ * PUT UPDATE CONTACT
+ */
+const updateContact = async ( req, res, next ) => {
+  const userId = new ObjectId( req.params.id );
+  const update = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection( 'contacts' )
+    .updateOne( {
+      _id: userId
+    }, {
+      $set: update
+    } );
+  res.setHeader( 'Content-Type', 'application/json' );
+  if ( result.modifiedCount === 1 ) {
+    res.status( 200 ).json( result.modifiedCount );
+  } else {
+    res.status( 404 ).json( ( result.error || "An error occured while updating contact" ) );
+  }
+};
+
+
+
+/************************
+ * DELETE CONTACT
+ */
+const deleteContact = async ( req, res, next ) => {
+  const userId = new ObjectId( req.params.id );
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection( 'contacts' )
+    .deleteOne( {
+      _id: userId
+    } );
+  res.setHeader( 'Content-Type', 'application/json' );
+
+  if ( result.deletedCount === 1 ) {
+    res.status( 200 ).json( result.deletedCount );
+  } else {
+    res.status( 404 ).json( {
+      error: 'No contact found'
+    } );
+  }
+}
+
+
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  postNew,
+  deleteContact,
+  updateContact
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express = require( 'express' );
 const router = express.Router();
 
-const contactsController = require('../controllers/contacts');
+const contactsController = require( '../controllers/contacts' );
 
-router.get('/', contactsController.getAll);
+/***************
+ * GET ALL CONTACTS
+ */
+router.get( '/', contactsController.getAll );
 
-router.get('/:id', contactsController.getSingle);
+router.get( '/:id', contactsController.getSingle );
+
+
+/***************
+ * POST NEW CONTACT
+ */
+router.post( '/', contactsController.postNew );
+
+
+/***************
+ * PUT UPDATE CONTACT
+ */
+router.put( '/:id', contactsController.updateContact );
+
+
+/***************
+ * DELETE CONTACT
+ */
+router.delete( '/:id', contactsController.deleteContact );
 
 module.exports = router;
