b11d879e8256638d7143f91f7b0c044a6a35b3ed
aamott <adam.amott@gmail.com>
2022-05-14 21:04:44 -0600

    testing cors

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const mongodb = require( './db/connect' );
+const cors = require( 'cors' );
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
   .use( bodyParser.json() )
-  .use( ( req, res, next ) => {
-    res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  } )
+  .use( cors() )
   .use( '/', require( './routes' ) );
 
 mongodb.initDb( ( err, mongodb ) => {
