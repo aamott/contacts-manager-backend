49d43e0e83bcba0bff09fbfd2a3852a1e62ac543
aamott <adam.amott@gmail.com>
2022-05-14 21:01:32 -0600

    testing allow-methods

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app
   .use( bodyParser.json() )
   .use( ( req, res, next ) => {
-    res.setHeader( 'Access-Control-Allow-Origin', "GET, POST, OPTIONS, PUT, DELETE" );
+    res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   } )
   .use( '/', require( './routes' ) );
